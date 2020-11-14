059a4f1c629c84045760171be7bbcfa8af12424c
maverick.lafont <mavericklafont@gmail.com>
2020-11-14 21:00:03 +0100

    add jwt token into Axios header

--- assets/app.js
+++ assets/app.js
@@ -2,5 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from "./Components/App";
+import Axios from "axios";
+
+Axios.defaults.headers.common['Authorization'] = 'Bearer ' + localStorage.getItem('token');
 
 ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
