c1674a17f5539354295da046a4f92b97d4e07fad
maverick.lafont <mavericklafont@gmail.com>
2020-11-14 20:59:38 +0100

    set up welcome index

--- assets/Components/Welcome/index.js
+++ assets/Components/Welcome/index.js
@@ -1,12 +1,26 @@
-import React from 'react'
+import React, {useState, useEffect, Fragment} from 'react'
+import Logout from "../Logout";
+import Quiz from "../Quiz";
 
-const Welcome = () => {
-    return(
+const Welcome = (props) => {
+
+    const [users, setUsers] = useState(false);
+
+    useEffect(() => {
+        localStorage.getItem('token') !== null ? setUsers(true) : props.history.push('/')
+    })
+    return users === true ? (
         <div className="quiz-bg">
             <div className="container">
-                Welcome
+                <Logout/>
+                <Quiz/>
             </div>
         </div>
+    ) : (
+        <Fragment>
+            <div className="loader"></div>
+            <p>Chargement ...</p>
+        </Fragment>
     )
 }
 
