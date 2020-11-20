4dd3642fa8dbaf8c5b8518669aa09a1114e59a6e
maverick.lafont <mavericklafont@gmail.com>
2020-11-20 14:32:22 +0100

    add progressBar, levels & data (question&answer)

--- assets/Components/Quiz/index.js
+++ assets/Components/Quiz/index.js
@@ -1,10 +1,57 @@
-import React from 'react';
+import React, {Component} from 'react';
+import {Data} from './data';
+import Levels from "./levels";
+import ProgressBar from "./progressBar";
 
-const Quiz = () => {
-    return (
-        <div>
-            Quiz
-        </div>
-    )
+class Quiz extends Component{
+    state = {
+        levelNames: ["debutant", "confirme", "expert"],
+        quizLevel: 0,
+        maxQuestion: 10,
+        storedQuestions: [],
+        question: null,
+        options: []
+    }
+
+    loadQuestions = level => {
+        const fetchedArrayQuiz = Data[0].quizz[level];
+        if(fetchedArrayQuiz.length >= this.state.maxQuestion){
+            const newArray = fetchedArrayQuiz.map(({ answer, ...keepRest}) => keepRest);
+
+            this.setState({
+                storedQuestions: newArray
+            })
+        } else {
+            console.log("Pas assez de question");
+        }
+    }
+
+    componentDidMount(){
+        this.loadQuestions(this.state.levelNames[this.state.quizLevel])
+    }
+
+    componentDidUpdate(prevProps, prevState){
+        if(this.state.storedQuestions !== prevState.storedQuestions){
+            this.setState({
+                question: this.state.storedQuestions[0]
+            })
+        }
+    }
+
+    render() {
+
+        return (
+            <div>
+                <Levels/>
+                <ProgressBar/>
+                <h2>Notre question quiz</h2>
+                <p className="answerOptions">Question 1</p>
+                <p className="answerOptions">Question 2</p>
+                <p className="answerOptions">Question 3</p>
+                <p className="answerOptions">Question 4</p>
+                <button className="btnSubmit">Suivant</button>
+            </div>
+        )
+    }
 }
 export default Quiz
