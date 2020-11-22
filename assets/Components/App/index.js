2b782b2853c40fdd9cc8b01274f8149277dd8a25
maverick.lafont <mavericklafont@gmail.com>
2020-11-22 18:49:16 +0100

    IconContext for all App component child

--- assets/Components/App/index.js
+++ assets/Components/App/index.js
@@ -9,21 +9,24 @@ import Login from "../Login";
 import Signup from "../Signup";
 import ErrorPage from "../ErrorPage";
 import {ProtectedRoute} from "../Route/protected.route";
+import {IconContext} from "react-icons";
 
 function App() {
     return (
         <Router>
-            <Header />
+            <IconContext.Provider value={{ style: { verticalAlign: 'middle' }}}>
+                <Header />
 
-            <Switch>
-                <Route exact path="/" component={Landing} />
-                <ProtectedRoute path="/welcome" component={Welcome} />
-                <Route path="/login" component={Login} />
-                <Route path="/signup" component={Signup} />
-                <Route component={ErrorPage} />
-            </Switch>
+                <Switch>
+                    <Route exact path="/" component={Landing} />
+                    <ProtectedRoute path="/welcome" component={Welcome} />
+                    <Route path="/login" component={Login} />
+                    <Route path="/signup" component={Signup} />
+                    <Route component={ErrorPage} />
+                </Switch>
 
-            <Footer/>
+                <Footer/>
+            </IconContext.Provider>
         </Router>
     )
 }
